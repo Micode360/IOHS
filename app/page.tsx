28ff510aa058b0e5181cfb904667eac7d9efdab7
Micode360 <abayemiracle@gmail.com>
2023-08-19 14:10:09 +0100

    changed color to blue

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,12 @@ export default function Home() {
           <div className="flex items-center">
             <div className="text-center md:text-left">
               <h2 className="text-[Open_Sans] text-[2rem] sm:text-[3rem] md:text-[3vw] lg:text-[3.5vw] xl:text-[3rem] font-[700] mb-8">
-                Welcome to Integrated Occupational Health Services!
+                <span className="font-[100]">Welcome to</span> Integrated Occupational Health Services!
               </h2>
               <div className="flex justify-center md:block">
                 <Link
                   href="/contact-us"
-                  className="flex justify-center items-center text-[1rem] bg-[#bc1414] hover:bg-[#ad1313] active:bg-[#bc1414] py-6 px-6 w-fit"
+                  className="flex justify-center items-center text-[1rem] bg-[#274fa7] hover:bg-[#244897] active:bg-[#274fa7] py-6 px-6 w-fit"
                 >
                   Contact Us
                 </Link>
@@ -28,6 +28,13 @@ export default function Home() {
           <div className="flex justify-center">
           </div>
         </div>
+      </section>
+
+      <section className="flex justify-center">
+            <div className="w-[90%] lg:w-[80%] grid grid-cols-2">
+              <div></div>
+              <div></div>
+            </div>
       </section>
 
       <Footer />
