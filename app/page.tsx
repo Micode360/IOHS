cd7e29e8e8d67be6dedfaa203f60c6d54a667497
Micode360 <abayemiracle@gmail.com>
2023-08-19 05:47:53 +0100

    header and footer created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Footer from '@/components/footer'
+import Header from '@/components/header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        IOHS ONLINE
+    <main>
+        <Header/>
+        <Footer/>
     </main>
   )
 }
---
+++ components/header.tsx
@@ -0,0 +1,61 @@
+"use client"
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Header () {
+    const [active, setActive] = useState<any>("");
+
+    useEffect(()=>{ 
+    const pageName = window.location.pathname.slice(1);
+    console.log(pageName, "pge")
+        setActive(pageName);
+    },[])
+    return (
+        <section>
+            <div className="ios-header py-8 flex items-center justify-center border-b">
+                <div className="w-[90%] lg:w-[80%] grid grid-cols-4 gap-4">
+                        <div className="col-span-2 flex items-center">
+                        <Image
+                            className="w-[20rem]"
+                            src="/logo_text.svg"
+                            width={65}
+                            height={65}
+                            alt="integrated occupational health services"
+                            />
+                        </div>
+                        <div className="ios-header-contacts">
+                            <p className="text-[#586e63] font-[700] mb-1">address: 6 ezimgbu crescent, presidential estate, gra phase iv, port harcourt, rivers state</p>
+                            <p className="text-[#586e63] font-[700] mb-1">Phone: +234 703 784 6079</p>
+                            <a className="text-[#586e63] font-[700] mb-1" href="mailto:info@iohs.com">Email: info@iohs.com</a>
+                        </div>
+                        <div className="ios-contact-us flex flex-col justify-center">
+                            <h3 className="font-[200] text-[1.3rem] mb-2">Contact Us</h3>
+                            <p className="text-[#586e63] text-[1.5rem] font-[700]">+234 703 784 6079</p>
+                        </div>
+                </div>
+            </div>
+            <div className="flex justify-center border-b">
+                <nav className="flex w-[90%] lg:w-[80%]">
+                    <Link className={`font-[700] ${active === ""? "bg-[#bc1414] text-white": "hover:bg-[#bc1414] hover:text-white  hover:shadow-lg"} py-4 md:py-6 w-[8rem] text-center relative`} href="/">
+                        Home
+                        <div className="bg-white absolute bottom-1 left-0 h-[0.28rem] right-0"></div>
+                    </Link>
+                    <Link className={`font-[700] ${active === "about-us"? "bg-[#bc1414] text-white": "hover:bg-[#bc1414] hover:text-white hover:shadow-lg"} py-4 md:py-6 w-[8rem] text-center relative`} href="/about-us">
+                        About Us
+                        <div className="bg-white absolute bottom-1 left-0 h-[0.28rem] right-0"></div>
+                    </Link>
+                    <Link className={`font-[700] ${active === "services"? "bg-[#bc1414] text-white": "hover:bg-[#bc1414] hover:text-white hover:shadow-lg"} py-4 md:py-6 w-[8rem] text-center relative`} href="/services">
+                            Services
+                        <div className="bg-white absolute bottom-1 left-0 h-[0.28rem] right-0"></div>
+                    </Link>
+                    <Link className={`font-[700] ${active === "contact"? "bg-[#bc1414] text-white": "hover:bg-[#bc1414] hover:text-white hover:shadow-lg"} py-4 md:py-6 w-[8rem] text-center relative`} href="/contact-us">
+                        Contact Us
+                        <div className="bg-white absolute bottom-1 left-0 h-[0.28rem] right-0"></div>
+                    </Link>
+                </nav>
+            </div>
+        </section>
+    );
+}
